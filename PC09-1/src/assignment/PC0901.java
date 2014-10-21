8a176f1e94a970deadff282e0e0e31631724dbb6
Matt <bored154@gmail.com>
2014-10-21 14:34:22 -0700

    Fixed formatting in PC09-1 and used the same method it in PC09-2

--- PC09-1/src/assignment/PC0901.java
+++ PC09-1/src/assignment/PC0901.java
@@ -25,16 +25,16 @@ public class PC0901 {
 		System.out.print("Enter worker's shift: ");
 		shift = input.nextInt();
 		
-		System.out.print("Enter worker's pay: ");
+		System.out.print("Enter worker's pay: $");
 		payRate = input.nextDouble();
 		
 		input.close();
 		
 		worker = new ProductionWorker(name, num, hireDate, shift, payRate);
 		
-		// TODO: Learn proper formatting.
-		System.out.printf("WORKER INFO:%nName\t\t\tNumber\tHireDate\tShift\t\tPayRate%n");
-		System.out.printf("%s\t%d\t%s\t\t%d\t\t%f%n", worker.getName(), worker.getNumber(), worker.getHireDate(), worker.getShift(), worker.getPayRate());
+		// TODO Learn proper formatting.
+		System.out.printf("WORKER INFO:%nName\t\t\tNumber\tHireDate   Shift  PayRate%n");
+		System.out.printf("%-23s %-7d %-10s %-6d $%.2f/hr %n", worker.getName(), worker.getNumber(), worker.getHireDate(), worker.getShift(), worker.getPayRate());
 		
 	}
 
